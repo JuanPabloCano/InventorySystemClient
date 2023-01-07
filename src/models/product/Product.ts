464b23be793700af0121def8185473b813446b9f
Juan Pablo Cano <canojuan0415@gmail.com>
2023-01-06 19:02:43 -0500

    Refactor: Unused properties removed

--- src/models/product/Product.ts
+++ src/models/product/Product.ts
@@ -1,17 +1,13 @@
 export class Product {
   enabled?: boolean;
   id?: string;
-  max?: number;
-  min?: number;
   name?: string;
   saleDetails?: [];
   stock?: number;
 
-  constructor(enabled?: boolean, id?: string, max?: number, min?: number, name?: string, saleDetails?: [], stock?: number) {
+  constructor(enabled?: boolean, id?: string, name?: string, saleDetails?: [], stock?: number) {
     this.enabled = enabled;
     this.id = id;
-    this.max = max;
-    this.min = min;
     this.name = name;
     this.saleDetails = saleDetails;
     this.stock = stock;
